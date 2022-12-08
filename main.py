016f074cb73e55bd54708108606623b9cefefd3a
Eva Lebedyuk <lebedyukeva@gmail.com>
2022-12-08 14:12:45 +0300

    like dislike commands

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sqlite3
 import telebot
 from ast import arguments
+import re
 
 bot = telebot.TeleBot("5987759126:AAHL6H-hGHt1EoQMUav_Jz8Eq1CkwPtCH7U")
 
-conn = sqlite3.connect('/Users/evalebedyuk/Desktop/food_bot_database.db', check_same_thread=False)
+conn = sqlite3.connect('food_bot_database.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
@@ -27,37 +28,141 @@ def start_message(message):
 def help_message(message):
     bot.send_message(message.chat.id, '''
 Функционал бота: */help*
-Добавить новый любимый ингредиент: */add_likes ингредиент*
-Добавить новый НЕ любимый ингредиент: */add_dislikes ингредиент*
-Убрать любимый ингредиент: */remove_likes ингредиент*
-Убрать НЕ любимый ингредиент: */remove_dislikes ингредиент*
 
-🤖🤖🤖 
-Прошу заметить, что при добавлении/удалении нескольких ингредиентов сразу */update_dislikes кефир молоко)*
-Происходит то же самое, что и при */update_dislikes сгущеное молоко*
+Добавить новый любимый ингредиент: */add_likes ингредиент1, ингредиент2, ...*
+Добавить новый НЕ любимый ингредиент: */add_dislikes ингредиент1, ингредиент2, ...*
+Убрать любимый ингредиент: */remove_likes ингредиент1, ингредиент2, ...*
+Убрать НЕ любимый ингредиент: */remove_dislikes ингредиент1, ингредиент2, ...*
 
-(держу в курсе)
-🤖🤖🤖 
+Посмотреть любимое: */my_likes*
+Посмотреть НЕ любимое: */my_dislikes*
 ''', parse_mode="markdown")
 
+
+def add_ingredient(food: str, pref: int, message):
+    pref_to_words = ["нелюбимых", "любимых"]
+    query = f'select likes_dislikes from preferences where user_id = \'{message.from_user.id}\' and food_name = \'{food}\''
+
+    previous_pref = cursor.execute(query).fetchone()
+
+    if previous_pref is None:
+        db_table_val(user_id=message.from_user.id, user_name=message.from_user.username, likes_dislikes=pref,
+                     food_name=food)
+        bot.send_message(message.chat.id,
+                         f"{food} теперь в вашей базе {pref_to_words[pref]}, @{message.from_user.username}")
+    elif previous_pref[0] == pref:
+        bot.send_message(message.chat.id,
+                         f"{food} уже есть в вашей базе {pref_to_words[pref]}, @{message.from_user.username}")
+    else:
+        bot.send_message(message.chat.id,
+                         f"НЕЛЬЗЯ одновременно и любить, и не любить {food}, @{message.from_user.username}")
+
+
+def remove_ingredient(food: str, pref: int, message):
+    pref_to_words = ["нелюбимых", "любимых"]
+    query = f'select id from preferences where user_id = \'{message.from_user.id}\' and food_name = \'{food}\''
+
+    id_to_be_removed = cursor.execute(query).fetchone()
+
+    if id_to_be_removed is None:
+        bot.send_message(message.chat.id,
+                         f"НЕЛЬЗЯ убрать то, чего нет, @{message.from_user.username}")
+
+    else:
+        cursor.execute(f'DELETE FROM preferences WHERE id = \'{id_to_be_removed[0]}\'')
+        conn.commit()
+
+        bot.send_message(message.chat.id,
+                         f"{food} больше не в вашей базе {pref_to_words[pref]}, @{message.from_user.username}")
 
 
 @bot.message_handler(commands=['add_likes'])
 def add_likes(message):
-    new_like = telebot.util.extract_arguments(message.text)
+    s = re.sub(r'\s+', ' ', telebot.util.extract_arguments(message.text))
+    likes_to_be_added = set(s.split(sep=","))
+    likes_to_be_added.discard(" ")
+    likes_to_be_added.discard("")
 
-    if len(new_like) == 0:
+    if len(likes_to_be_added) == 0:
         bot.send_message(message.chat.id, '''
 К сожалению, я не умею читать мысли...
 Введите название ващего любимого ингредиента!
-        ''')
+               ''')
         return
 
-    db_table_val(user_id=message.from_user.id, user_name=message.from_user.username, likes_dislikes=1,
-                 food_name=new_like)
-    bot.send_message(message.chat.id, f"{new_like.strip()} теперь в вашей базе, @{message.from_user.username}")
+    for like in likes_to_be_added:
+        add_ingredient(like.strip().lower(), 1, message)
+
+
+@bot.message_handler(commands=['add_dislikes'])
+def add_dislikes(message):
+    s = re.sub(r'\s+', ' ', telebot.util.extract_arguments(message.text))
+    dislikes_to_be_added = set(s.split(sep=","))
+    dislikes_to_be_added.discard(" ")
+    dislikes_to_be_added.discard("")
+
+    if len(dislikes_to_be_added) == 0:
+        bot.send_message(message.chat.id, '''
+К сожалению, я не умею читать мысли...
+Введите название ващего нелюбимого ингредиента!
+               ''')
+        return
+
+    for dislike in dislikes_to_be_added:
+        add_ingredient(dislike.strip().lower(), 0, message)
+
+
+@bot.message_handler(commands=['remove_likes'])
+def remove_likes(message):
+    s = re.sub(r'\s+', ' ', telebot.util.extract_arguments(message.text))
+    likes_to_be_removed = set(s.split(sep=","))
+    likes_to_be_removed.discard(" ")
+    likes_to_be_removed.discard("")
+
+    if len(likes_to_be_removed) == 0:
+        bot.send_message(message.chat.id, '''
+К сожалению, я не умею читать мысли...
+Введите название ващего любимого ингредиента!
+               ''')
+        return
+
+    for like in likes_to_be_removed:
+        remove_ingredient(like.strip().lower(), 1, message)
+
+
+@bot.message_handler(commands=['remove_dislikes'])
+def remove_dislikes(message):
+    s = re.sub(r'\s+', ' ', telebot.util.extract_arguments(message.text))
+    dislikes_to_be_removed = set(s.split(sep=","))
+    dislikes_to_be_removed.discard(" ")
+    dislikes_to_be_removed.discard("")
+
+    if len(dislikes_to_be_removed) == 0:
+        bot.send_message(message.chat.id, '''
+К сожалению, я не умею читать мысли...
+Введите название ващего нелюбимого ингредиента!
+               ''')
+        return
+
+    for dislike in dislikes_to_be_removed:
+        remove_ingredient(dislike.strip().lower(), 0, message)
+
 
 @bot.message_handler(commands=['my_likes'])
-def add_likes(message):
+def my_likes(message):
+    query = f'select food_name from preferences where user_id =  \'{message.from_user.id} \' and likes_dislikes = 1'
+    likes = cursor.execute(query).fetchall()
+    bot.send_message(message.chat.id,
+                     f"Любимые ингредиенты @{message.from_user.username}:\n\n" + "\n".join(map(lambda x: x[0], likes)))
+
+
+@bot.message_handler(commands=['my_dislikes'])
+def my_likes(message):
+    query = f'select food_name from preferences where user_id =  \'{message.from_user.id} \' and likes_dislikes = 0'
+    likes = cursor.execute(query).fetchall()
+    bot.send_message(message.chat.id,
+                     f"Нелюбимые ингредиенты @{message.from_user.username} 🤢:\n\n" + "\n".join(
+                         map(lambda x: x[0], likes)))
+
 
 bot.polling(none_stop=True)
